f8c690fe8e23fc6153c6f6d28c9ee83c8eaf6a05
Sadia Islam <sadiaislam@Sadias-MacBook-Pro.local>
2024-06-01 15:02:05 +0200

    php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is first class of PHP </h1>
+</body>
+</html>
+ 
+
+<?php
+
+ $number1 = 30;
+ $number2= 20;
+
+ $result = $number1 + $number2;
+
+ echo $result;
+  
+ echo '<h1> Hello Bangladesh </h1)'
+ 
+?>
